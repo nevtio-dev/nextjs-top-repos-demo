da1e77ffa7240578cdbf5ede4b47b4ce493164f6
Gordon Cheng <nevtio.dev@gmail.com>
2023-06-14 11:03:09 +0930

    Fix lint issue

--- src/app/api/repos/route.ts
+++ src/app/api/repos/route.ts
@@ -1,4 +1,3 @@
-
 import { NextRequest, NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
@@ -6,11 +5,11 @@ import { listGithubRepos } from '@/lib/graphql'
 
 export async function GET(req: NextRequest) {
     const session = await getServerSession(authOptions)
-    if (!session)
-        return new NextResponse('Unauthorised', { status: 401 })
+    if (!session) return new NextResponse('Unauthorised', { status: 401 })
 
     const cursor = new URL(req.url).searchParams.get('cursor')
     const data = await listGithubRepos(cursor || '')
+
     // const data = [{ value:1 }, { value:2 }, { value:3 }]
 
     return NextResponse.json(data)
